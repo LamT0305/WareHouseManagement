e06ddb37424fd2723b5b43b9e105b7df51c0bbda
LamT0305 <lam45883@gmail.com>
2023-04-22 01:18:25 +0700

    Set up API CRUD for category

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -5,38 +5,21 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Categories;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $categories = Categories::all();
+
         return response()->json([
             'success' => true,
             'data' => $categories
-        ]);
+        ], 200);
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $category = Categories::create($request->all());
-        return response()->json([
-            'success' => true,
-            'data' => $category
-        ], 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id)
     {
         $category = Categories::find($id);
 
@@ -50,26 +33,51 @@ class CategoryController extends Controller
         return response()->json([
             'success' => true,
             'data' => $category
-        ]);
+        ], 200);
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Categories $category)
+    public function store(Request $request)
     {
-        $category->update($request->all());
+        $this->validate($request, [
+            'name' => 'required|unique:categories,name'
+        ]);
+
+        $category = Categories::create([
+            'name' => $request->name
+        ]);
+
         return response()->json([
             'success' => true,
             'data' => $category
-        ]);
+        ], 201);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function update(Request $request, $id)
+    {
+        $category = Categories::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Category not found'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $product = Categories::findOrFail($id);
+        $product->update($request->all());
+
+        return response()->json($product, 200);
+    }
+
+    public function destroy($id)
     {
         $category = Categories::find($id);
 
@@ -85,6 +93,6 @@ class CategoryController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Category deleted successfully'
-        ]);
+        ], 200);
     }
 }
