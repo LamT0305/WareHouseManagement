e0c61e75294fcdeba81ac98304ce3ad64e5131db
TuanBonn <118041345+TuanBonn@users.noreply.github.com>
2023-04-22 15:03:01 +0700

    create ui design

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    return view('homepage.index');
 });
